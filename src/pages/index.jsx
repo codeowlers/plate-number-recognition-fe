c0d0aa291e1db0a16d24e1a6a79edef73ab74687
developer-sidani <developer.sidani@gmail.com>
2022-04-19 01:20:37 +0300

    final gui

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import {
   Box, Button,
   Card,
   CardContent,
-  Container,
+  Container, Divider,
   FormControl,
   Grid,
   IconButton,
@@ -18,16 +18,39 @@ import Head from 'next/head'
 import Image from 'next/image'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import { toast } from 'react-hot-toast'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import { getData } from '../api'
 import { FileDropzone } from '../components/file-drop-zone'
 import { UploadFile } from '../hooks'
 import { Logo } from '../components/logo'
 
 const HomePage = () => {
+  const formik = useFormik({
+    initialValues: {
+      epochs: 0,
+      submit: null,
+    },
+    validationSchema: Yup.object({
+      epochs: Yup.number().min(0, 'At least 0').max(7, 'At most 7').required('Required'),
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        // NOTE: Make API request
+        toast.success('Product created!')
+        console.log(values)
+      } catch (err) {
+        console.error(err)
+        toast.error('Something went wrong!')
+        helpers.setStatus({ success: false })
+        helpers.setErrors({ submit: err.message })
+        helpers.setSubmitting(false)
+      }
+    },
+  })
   const [files, setFiles] = useState([])
   const [response, setResponse] = useState({})
   const [loading, setLoading] = useState(false)
-  const [epochs, setEpochs] = useState('')
   const [photo, handlePhotoUpload] = UploadFile({
     location: 'python',
     fileTypes: ['image/jpg', 'image/jpeg', 'image/png'],
@@ -40,11 +63,11 @@ const HomePage = () => {
   const src = useMemo(() => photo.url, [photo.url])
   // const responseUrl = useMemo(() => response?.url, [response.url])
   const getUrl = useCallback(async (url) => {
-    if (epochs >= 0 && epochs < 7) {
+    if (formik.values.epochs >= 0 && formik.values.epochs < 7) {
       if (url) {
         setLoading(true)
         try {
-          const res = await getData(epochs, url)
+          const res = await getData(formik.values.epochs, url)
           console.log(res)
           return res
         } catch (err) {
@@ -56,9 +79,9 @@ const HomePage = () => {
     } else {
       setFiles([])
       photo.setUrl('')
-      setEpochs('')
+      await formik.setFieldValue('epochs', 0)
     }
-  }, [epochs, photo])
+  }, [formik, photo])
   const handleDrop = (newFiles) => {
     // handlePhotoUpload(newFiles)
     setFiles(newFiles)
@@ -66,9 +89,12 @@ const HomePage = () => {
   useEffect(() => {
     if (photo.url && !response.predictions) {
       getUrl(photo.url).then(({
+        // eslint-disable-next-line camelcase
         plate_contour,
+        // eslint-disable-next-line camelcase
         plate_segmented,
         predictions,
+        // eslint-disable-next-line camelcase
         plate_number,
       }) => {
         setResponse({
@@ -140,6 +166,9 @@ const HomePage = () => {
         </Card>
         <Card sx={{ mt: 3 }}>
           <CardContent>
+            <form
+              onSubmit={formik.handleSubmit}
+            >
             <Grid
               container
               spacing={3}
@@ -177,22 +206,27 @@ const HomePage = () => {
                   </Grid>
                   <Grid item xs={12}>
                     <TextField
+                      error={Boolean(formik.touched.epochs && formik.errors.epochs)}
+                      helperText={formik.touched.epochs && formik.errors.epochs}
+                      name="epochs"
+                      onBlur={formik.handleBlur}
                       fullWidth
                       id="outlined-basic"
                       label="Epochs"
                       variant="outlined"
                       type="number"
-                      onChange={(e) => setEpochs(e.target.value)}
-                      value={epochs}
+                      onChange={formik.handleChange}
+                      value={formik.values.epochs}
                     />
                   </Grid>
                   <Grid item xs={12}>
                     <Button
+                      type="submit"
                       onClick={() => {
                         handlePhotoUpload(files)
                       }}
                       variant="contained"
-                      disabled={files.length === 0 || epochs.length === 0 || photo.active || loading}
+                      disabled={response?.plate_number || files.length === 0 || formik.values.epochs.length === 0 || photo.active || loading || Boolean(formik.touched.epochs && formik.errors.epochs)}
                     >
                       Send
                     </Button>
@@ -200,125 +234,134 @@ const HomePage = () => {
                 </Grid>
               </Grid>
             </Grid>
+            </form>
           </CardContent>
         </Card>
         {photo?.url && (
           <Card sx={{ mt: 3 }}>
             <CardContent>
-              <Grid
-                container
-                spacing={3}
-              >
-                <Grid
-                  item
-                  md={4}
-                  xs={12}
-                >
-                  <Typography variant="h6">
-                    Result
-                  </Typography>
-                  <Typography
-                    color="textSecondary"
-                    variant="body2"
-                    sx={{ mt: 1 }}
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <Grid
+                    container
+                    spacing={3}
                   >
-                    Here is the image that you submitted and it&apos;s output
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  md={8}
-                  xs={12}
-                >
-                  <Grid container spacing={3}>
-                    <Grid item xs={12}>
+                    <Grid
+                      item
+                      md={4}
+                      xs={12}
+                    >
+                      <Typography variant="h6">
+                        Result
+                      </Typography>
                       <Typography
                         color="textSecondary"
-                        variant="body1"
-                        sx={{ my: 1 }}
+                        variant="body2"
+                        sx={{ mt: 1 }}
                       >
-                        Original Image
+                        Here is the image that you submitted and it&apos;s output
                       </Typography>
-                      <Image
-                        loader={() => src}
-                        src={src}
-                        layout="responsive"
-                        height={100}
-                        width={100}
-                      />
                     </Grid>
-                    {loading ? (<Grid item xs={12}><LinearProgress color="success" /></Grid>) : (
-                      <Grid item xs={12}>
-                        <Grid container spacing={2}>
-                          <Grid item xs={12}>
-                            <Typography
-                              color="textSecondary"
-                              variant="body1"
-                              sx={{ my: 1 }}
-                            >
-                              Plate Contour
-                            </Typography>
-                            <img
-                              src={response?.plate_contour}
-                              alt="response"
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <Typography
-                              color="textSecondary"
-                              variant="body1"
-                              sx={{ my: 1 }}
-                            >
-                              Plate Segmented
-                            </Typography>
-                            <img
-                              src={response?.plate_segmented}
-                              alt="response"
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <Typography
-                              color="textSecondary"
-                              variant="body1"
-                              sx={{ my: 1 }}
-                            >
-                              Predictions
-                            </Typography>
-                            <img
-                              src={response?.predictions}
-                              alt="response"
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <FormControl fullWidth variant="outlined">
-                              <InputLabel htmlFor="outlined-adornment-password">Plate Numbers</InputLabel>
-                              <OutlinedInput
-                                disabled
-                                id="outlined-adornment-password"
-                                type="text"
-                                value={response?.plate_number || ''}
-                                endAdornment={(
-                                  <Tooltip title="Copy">
-                                    <InputAdornment position="end">
-                                      <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={() => { navigator.clipboard.writeText(response?.plate_number).then(() => toast.success('Plate Number Copied to clipboard')) }}
-                                        edge="end"
-                                      >
-                                        <ContentCopyIcon />
-                                      </IconButton>
-                                    </InputAdornment>
-                                  </Tooltip>
-                                )}
-                                label="Password"
-                              />
-                            </FormControl>
-                          </Grid>
+                    <Grid
+                      item
+                      md={8}
+                      xs={12}
+                    >
+                      <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                          <Typography
+                            color="textSecondary"
+                            variant="body1"
+                            sx={{ my: 1 }}
+                          >
+                            Original Image
+                          </Typography>
+                          <Image
+                            loader={() => src}
+                            src={src}
+                            layout="responsive"
+                            height={100}
+                            width={100}
+                          />
                         </Grid>
                       </Grid>
-                    )}
+                    </Grid>
                   </Grid>
                 </Grid>
+                {loading ? (<Grid item xs={12}><LinearProgress color="success" /></Grid>) : (
+                  <Grid item xs={12}>
+                    <Divider sx={{ my: 2 }} />
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <Typography
+                          color="textSecondary"
+                          variant="body1"
+                          sx={{ my: 1 }}
+                        >
+                          Plate Contour
+                        </Typography>
+                        <img
+                          style={{ backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}
+                          src={response?.plate_contour}
+                          alt="response"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography
+                          color="textSecondary"
+                          variant="body1"
+                          sx={{ my: 1 }}
+                        >
+                          Plate Segmented
+                        </Typography>
+                        <img
+                          style={{ backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}
+                          src={response?.plate_segmented}
+                          alt="response"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography
+                          color="textSecondary"
+                          variant="body1"
+                          sx={{ my: 1 }}
+                        >
+                          Predictions
+                        </Typography>
+                        <img
+                          style={{ backgroundSize: 'contain', backgroundRepeat: 'no-repeat' }}
+                          src={response?.predictions}
+                          alt="response"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <FormControl fullWidth variant="outlined">
+                          <InputLabel htmlFor="outlined-adornment-password">Plate Numbers</InputLabel>
+                          <OutlinedInput
+                            disabled
+                            id="outlined-adornment-password"
+                            type="text"
+                            value={response?.plate_number || ''}
+                            endAdornment={(
+                              <Tooltip title="Copy">
+                                <InputAdornment position="end">
+                                  <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => { navigator.clipboard.writeText(response?.plate_number).then(() => toast.success('Plate Number Copied to clipboard')) }}
+                                    edge="end"
+                                  >
+                                    <ContentCopyIcon />
+                                  </IconButton>
+                                </InputAdornment>
+                              </Tooltip>
+                            )}
+                            label="Password"
+                          />
+                        </FormControl>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                )}
               </Grid>
             </CardContent>
           </Card>
